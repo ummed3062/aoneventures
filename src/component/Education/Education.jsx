481f0a2b1aea32ed5d185094fae5b205847dffa3
Ummed Singh <52965044+ummed3062@users.noreply.github.com>
2025-04-07 22:11:18 +0530

    Update Education.jsx

--- src/component/Education/Education.jsx
+++ src/component/Education/Education.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 
